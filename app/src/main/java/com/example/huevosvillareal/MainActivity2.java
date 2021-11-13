f82312920b25c9602ed478676f105f18c2a21ec7
JhonFredyAlvarez <jfao377@gmail.com>
2021-11-12 23:58:39 -0500

    primercommit

---
+++ app/src/main/java/com/example/huevosvillareal/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.huevosvillareal;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
