fcbccb4d22eecd63d2b4db0611bd56833df2874e
JhonFredyAlvarez <jfao377@gmail.com>
2021-11-15 17:43:33 -0500

    pase el proyecto de la ramma jhon fredy a master

---
+++ Huevos_Villa_Real/app/src/main/java/com/example/huevos_villa_real/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.huevos_villa_real;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        public void onClick(View view) {
+
+        Intent i = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(i);//comentario1
+    }
+}
